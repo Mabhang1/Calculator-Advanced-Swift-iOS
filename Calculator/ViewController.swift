3731495e0270651e932a965c961c89b32ba36fee
Abhang Mane <maneabhang1@gmail.com>
2024-01-12 17:23:58 +0530

    Initial Commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,11 +12,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        isFinishedTypingNumber = true
+        guard let number = Double(displayLabel.text!) else{
+            fatalError("Error")
+        }
+        
+        if let calcMethod = sender.currentTitle{
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            }
+            else if calcMethod == "AC"{
+                displayLabel.text = "0"
+            }
+            else if calcMethod == "%"{
+                displayLabel.text = String(number * 0.01)
+            }
+        }
     
     }
 
@@ -24,7 +40,24 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber{
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }
+            else{
+                if numValue == "."{
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Error")
+                    }
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    if !isInt{
+                        return
+                    }
+                }
+                displayLabel.text = displayLabel.text! + numValue
+            }
+        }
     }
 
 }
